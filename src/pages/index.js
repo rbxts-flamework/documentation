d54e94c63246306084ce4dba6e872dd80faff35a
cheetiedotpy <77159113+g1mmethemoney@users.noreply.github.com>
2024-03-08 19:30:56 +0300

    fix the social card

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,11 +29,10 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-	const { siteConfig } = useDocusaurusContext();
 	return (
 		<Layout
-			title={`Hello from ${siteConfig.title}`}
-			description="Description will go into a meta tag in <head />">
+			title={`Flamework`}
+			description="An extensible game framework for roblox-ts.">
 			<HomepageHeader />
 			<main>
 				<HomepageFeatures />
